733822137b3b2f46a49ff4ef3d0a739baa56cf28
Thibault Person <tperson@siradel.com>
2017-05-09 14:13:11 +0200

    Extract data from zipfile

--- emulators/advmame_emulator.py
+++ emulators/advmame_emulator.py
@@ -1,14 +1,23 @@
 import logging
+from os.path import join
 from generic_emulator import GenericEmulator
 
 
 
 class AdvmameEmulator(GenericEmulator):
+    commands = {
+        'generate_roms_file': '',
+        'run_rom': ''
+    }
 
     def __init__(self, execPath, romsPath):
         super(AdvmameEmulator, self).__init__(execPath, romsPath)
 
+    def generateSupportedRomFile(self):
+        logger = logging.getLogger('pyaf')
+        logger.info('Generate file of supported roms list')
 
     def checkRomValidity(self, romPath):
         logger = logging.getLogger('pyaf')
         logger.info('Check validity of rom: %s', romPath)
+        super(AdvmameEmulator, self).extractDataFromZipFile(join(self.romsPath, romPath))
--- emulators/generic_emulator.py
+++ emulators/generic_emulator.py
@@ -1,6 +1,7 @@
 import logging
+import zipfile
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, join, splitext
 
 class GenericEmulator(object):
 
@@ -14,13 +15,20 @@ class GenericEmulator(object):
         files = [f for f in listdir(self.romsPath) if isfile(join(self.romsPath, f))]
         logger.info('Found %d files.', len(files))
         for f in files:
-            self.checkRomValidity(f)
+            filename, fileExt = splitext(f)
+            if fileExt == '.zip':
+                self.checkRomValidity(f)
 
     def checkRomValidity(self, romPath):
         raise NotImplementedError
 
-
-
-
     def extractDataFromZipFile(self, zipFilePath):
-        
+        logger = logging.getLogger('pyaf')
+        data = []
+        logger.info('Files in: %s', zipFilePath)
+        with zipfile.ZipFile(zipFilePath, 'r') as romZip:
+            infos = romZip.infolist()
+            for info in infos:
+                logger.info('Filename: %s  CRC: %s', info.filename, info.CRC)
+                data.append({'filename': info.filename, 'crc': info.CRC})
+        return data
